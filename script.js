e4af512e673572aa2a5eaa975904d7442b59a0b4
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-29 13:41:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const parseCode = (str) => {
   // your code here
+	let words = str.split(/00*/g);
+	let obj = {
+		firstName: "",
+		lastName:"",
+		id: ""
+	for(let key in obj){
+		obj[key] = words.shift();
+	}
+	return obj;
 };
 
 // Do not change the code below
